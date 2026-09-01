079f6c93bd7d71b123b1bbdbcd4716cd1c1dd86b
BiggerToast <zeekcasillas7@gmail.com>
2026-09-01 09:14:57 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    float temp;
+    char _letter = 'F';
+    string favColor = "grey";
+    double pi;
+    string farArtist;
+    
+    cout << "enter value of pie:";
+    cin >> pi;
+    
+    cout << "temp:" << temp << endl;
+    cout << "favColor:" << favColor <<endl;
+    cout << "pi:" << pi <<endl;
+
+    return 0;
+}
