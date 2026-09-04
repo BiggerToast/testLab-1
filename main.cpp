efe92ce8b06f583dd10d3ec96d3b82105cc01758
BiggerToast <zeekcasillas7@gmail.com>
2026-09-04 12:31:53 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,28 +1,29 @@
 /******************************************************************************
 
 Welcome to GDB Online.
-  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
-  Code, Compile, Run and Debug online from anywhere in world.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
 #include <iostream>
-
+#include <iomanip>
 using namespace std;
 int main()
 {
-    float temp;
-    char _letter = 'F';
-    string favColor = "grey";
-    double pi;
-    string farArtist;
+    int multi;
     
-    cout << "enter value of pie:";
-    cin >> pi;
-    
-    cout << "temp:" << temp << endl;
-    cout << "favColor:" << favColor <<endl;
-    cout << "pi:" << pi <<endl;
-
+    cout << "enter the number you multiplied:";
+    cin >> multi;
+    cout << setw(2) << multi << setw(4) << "x 1=" << setw(3) << multi * 1 << endl <<
+     setw(2) << multi << setw(4) << "x 1=" << setw(3) << multi * 2 << endl <<
+     setw(2) << multi << setw(4) << "x 1=" << setw(3) << multi * 3 << endl <<
+     setw(2) << multi << setw(4) << "x 1=" << setw(3) << multi * 4 << endl <<
+     setw(2) << multi << setw(4) << "x 1=" << setw(3) << multi * 5 << endl <<
+     setw(2) << multi << setw(4) << "x 1=" << setw(3) << multi * 6 << endl <<
+     setw(2) << multi << setw(4) << "x 1=" << setw(3) << multi * 7 << endl <<
+     setw(2) << multi << setw(4) << "x 1=" << setw(3) << multi * 8 << endl <<
+     setw(2) << multi << setw(4) << "x 1=" << setw(3) << multi * 9 << endl <<
+     setw(2) << multi << setw(4) << "x 1=" << setw(3) << multi * 10 << endl;
     return 0;
 }
